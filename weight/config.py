1e8eb84dab88bb4332786352879ea300899c840c
richardruancw <ruanchuanwei@gmail.com>
2020-09-27 23:07:05 -0700

    use normalized function margin; example usage for linear separable data;

--- weight/config.py
+++ weight/config.py
@@ -4,9 +4,9 @@ class Config:
     axis_limit = 4 # Limit for both x and y axis
     size = 1024 # Number of points
     dis2origin = 3 
-    batch_size=64 
+    batch_size=1024
     buffer_size=50
-    l2 = 0.001
+    l2 = 0.000
     resolution=200 # Number of steps used in linspace
     
     x_span = np.linspace(-1 * axis_limit, axis_limit, resolution)
